7abce5d6a985834b0bdc3452806eaf595c474fed
Brian Abidin <Brian.abidin1@gmail.com>
2024-10-29 14:44:54 -0400

    testing if Linkedlistand Node classes are working as intended

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
 import Linkedlist from "./linkedlist.js";
+import Node from "./node.js";
 
-const node = new Linkedlist();
-node.append("hello");
+const newnode = new Node(3);
+const node2 = new Node(2);
+const node3 = new Node(1);
+const newList = new Linkedlist();
+newList.append(newnode);
+newList.prepend(node2);
+newList.prepend(node3);
+console.log(newList.nextNode.nextNode.nextNode);
